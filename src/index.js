e620562625f1e7508cab8dade98d0a756c86c2f7
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-03-02 15:47:31 +0100

    Remeber the user

--- src/index.js
+++ src/index.js
@@ -8,12 +8,23 @@ import thunk from "redux-thunk";
 import AppContainer from "./app-container";
 
 import reducers from "./reducers";
+import { login } from "./actions";
 
 //  npm install bootstrap --save
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+if (localStorage.user) {
+  try {
+    const user = JSON.parse(localStorage.user);
+    store.dispatch(login(user));
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  }
+}
 
 ReactDOM.render(
   <Provider store={store}>
